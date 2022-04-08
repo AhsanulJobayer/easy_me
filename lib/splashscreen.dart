1552816a0d978c64f3f2523aaf0179acc2e32736
Tahmid Imtiaz Imu <tahmidimtiaz11807048@gmail.com>
2022-04-08 17:31:54 +0600

    Solved

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -3,6 +3,7 @@ import 'package:easy_me/homepage.dart';
 import 'package:easy_me/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();

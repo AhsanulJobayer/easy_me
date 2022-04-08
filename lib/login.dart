3d6749faf4fee7ba0e74e3c7d25ebc8bebcec5b3
Tahmid Imtiaz Imu <tahmidimtiaz11807048@gmail.com>
2022-04-08 17:29:41 +0600

    Alright now

--- lib/login.dart
+++ lib/login.dart
@@ -2,18 +2,6 @@ import 'package:easy_me/signup.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'EASY.ME';
-
 class login extends StatelessWidget {
   const login({Key? key}) : super(key: key);
 

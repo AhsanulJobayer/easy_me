3f6cdb62cd31d566ae718c72eb66f5d91e6cb894
Tahmid Imtiaz Imu <tahmidimtiaz11807048@gmail.com>
2022-04-12 19:44:25 +0600

    Auth done

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -28,24 +28,20 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 3),
-            ()=>Navigator.pushReplacement(context,
-            MaterialPageRoute(builder:
-                (context) =>
-                login()
-            )
-        )
-    );
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => login())));
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
-        child:Image.asset('assets/images/logo/logo.png')
-    );
+        color: Colors.white, child: Image.asset('assets/images/logo/logo.png'));
   }
 }
